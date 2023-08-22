2602ffedd512e13655c3cff0b15f7f25a33934e6
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-22 13:21:23 +0000

    Correct linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(content:"Hello there! How is it going?")
+Message.create(content:"Hey, good to see you!")
+Message.create(content:"Hey, it's a pleasure to connect with you!")
+Message.create(content:"Greetings and salutations!")
+Message.create(content:"Hey, good to see you!")
